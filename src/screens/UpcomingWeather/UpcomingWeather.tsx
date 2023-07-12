acb88c7cc57889eaf667e8fe86b7390e40d88125
Erdem Alptuğ Öksüz <81254185+erdemalptugoksuz@users.noreply.github.com>
2023-07-12 16:15:44 +0300

    Added UpcomingWeather screen

---
+++ src/screens/UpcomingWeather/UpcomingWeather.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+import styles from './UpcomingWeather.style';
+
+const UpcomingWeather = () => {
+  return (
+    <View>
+      <Text>UpcomingWeather</Text>
+    </View>
+  );
+};
+
+export default UpcomingWeather;
