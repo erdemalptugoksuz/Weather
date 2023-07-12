29665b7be2607bd288aa1e87d9587f2da9dd144f
Erdem Alptuğ Öksüz <81254185+erdemalptugoksuz@users.noreply.github.com>
2023-07-12 18:14:46 +0300

    Update Upcoming Item component

--- src/components/UpcomingItem/UpcomingItem.style.ts
+++ src/components/UpcomingItem/UpcomingItem.style.ts
@@ -1,3 +1,25 @@
 import {StyleSheet} from 'react-native';
 
-export default StyleSheet.create({});
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+    marginHorizontal: 30,
+    paddingVertical: 10,
+    justifyContent: 'space-between',
+  },
+  day: {
+    fontSize: 20,
+    fontStyle: 'italic',
+    color: 'gray',
+  },
+  weather: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  hour: {
+    marginLeft: 10,
+    color: 'gray',
+  },
+});
--- src/components/UpcomingItem/UpcomingItem.tsx
+++ src/components/UpcomingItem/UpcomingItem.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import {View, Text} from 'react-native';
+import moment from 'moment';
+import Icon from 'react-native-vector-icons/Feather';
 
 import styles from './UpcomingItem.style';
+import {weatherType} from '../../utilities/weatherType';
 
-const UpcomingItem = () => {
+const UpcomingItem = (props: any) => {
+  const {condition, dt_txt, min, max} = props;
   return (
-    <View>
-      <Text>UpcomingItem</Text>
+    <View style={styles.container}>
+      <Text style={styles.day}>{moment(dt_txt).format('ddd')}</Text>
+      <View style={styles.weather}>
+        <Icon name={weatherType[condition].icon} size={25} color="black" />
+        <Text style={styles.hour}>{moment(dt_txt).format('hh:mm')}</Text>
+      </View>
+      <Text>{`${Math.round(min)}° / ${Math.round(max)}°`}</Text>
     </View>
   );
 };
