0bd8d6f24a82d878250e94752fb0e850d934dc8f
Erdem Alptuğ Öksüz <81254185+erdemalptugoksuz@users.noreply.github.com>
2023-07-12 16:15:27 +0300

    Added CurrentWeather screen

---
+++ src/screens/CurrentWeather/CurrentWeather.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+import styles from './CurrentWeather.style';
+
+const CurrentWeather = () => {
+  return (
+    <View>
+      <Text>CurrentWeather</Text>
+    </View>
+  );
+};
+
+export default CurrentWeather;
