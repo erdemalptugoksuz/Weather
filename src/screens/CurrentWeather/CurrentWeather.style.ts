a13779dd1ba7985f4b88da31bb31577dc91ffbd6
Erdem Alptuğ Öksüz <81254185+erdemalptugoksuz@users.noreply.github.com>
2023-07-12 17:36:44 +0300

    Added Current Weather screen

--- src/screens/CurrentWeather/CurrentWeather.style.ts
+++ src/screens/CurrentWeather/CurrentWeather.style.ts
@@ -1,3 +1,51 @@
 import {StyleSheet} from 'react-native';
 
-export default StyleSheet.create({});
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  locationContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginHorizontal: 30,
+    marginTop: 30,
+  },
+  cityName: {
+    fontSize: 22,
+  },
+  countryName: {
+    fontSize: 18,
+    color: 'gray',
+  },
+  iconContainer: {
+    alignItems: 'center',
+    marginTop: 100,
+  },
+  weatherContainer: {
+    flex: 1,
+    alignItems: 'center',
+    marginTop: 100,
+  },
+  temp: {
+    fontSize: 40,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  weatherInfo: {
+    flexDirection: 'row',
+  },
+  weatherMessage: {
+    marginHorizontal: 30,
+    marginBottom: 30,
+  },
+  description: {
+    fontSize: 28,
+    fontWeight: 'bold',
+  },
+  message: {
+    fontSize: 18,
+    fontStyle: 'italic',
+    color: 'gray',
+  },
+});
--- src/screens/CurrentWeather/CurrentWeather.tsx
+++ src/screens/CurrentWeather/CurrentWeather.tsx
@@ -1,13 +1,48 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {SafeAreaView, View, Text} from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
 
 import styles from './CurrentWeather.style';
+import {weatherType} from '../../utilities/weatherType';
+import {WeatherInfo} from '../../components';
 
-const CurrentWeather = () => {
+const CurrentWeather = ({weatherData}: any) => {
+  const {
+    main: {temp, feels_like, temp_max, temp_min},
+    weather,
+  } = weatherData.list[0];
+  const {country, name} = weatherData.city;
+
+  const weatherCondition = weather[0].main;
+
   return (
-    <View>
-      <Text>CurrentWeather</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.locationContainer}>
+        <Text style={styles.cityName}>{name}</Text>
+        <Text style={styles.countryName}>{country}</Text>
+      </View>
+      <View style={styles.iconContainer}>
+        <Icon
+          name={weatherType[weatherCondition].icon}
+          color={'tomato'}
+          size={100}
+        />
+      </View>
+      <View style={styles.weatherContainer}>
+        <Text style={styles.temp}>{temp}°</Text>
+        <View style={styles.weatherInfo}>
+          <WeatherInfo value={feels_like} name={'thermometer'} />
+          <WeatherInfo value={temp_max} name={'upload-cloud'} />
+          <WeatherInfo value={temp_min} name={'download-cloud'} />
+        </View>
+      </View>
+      <View style={styles.weatherMessage}>
+        <Text style={styles.description}>{weather[0]?.description}</Text>
+        <Text style={styles.message}>
+          {weatherType[weatherCondition]?.message}
+        </Text>
+      </View>
+    </SafeAreaView>
   );
 };
 
