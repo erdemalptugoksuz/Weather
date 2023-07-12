0bd405dfa56e7dee11f91779cf2449a8aa596316
Erdem Alptuğ Öksüz <81254185+erdemalptugoksuz@users.noreply.github.com>
2023-07-12 14:12:04 +0300

    Update Router.tsx

---
+++ src/Router.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {SafeAreaView, Text} from 'react-native';
+
+const Router = () => {
+  return (
+    <SafeAreaView>
+      <Text>Router</Text>
+    </SafeAreaView>
+  );
+};
+
+export default Router;
