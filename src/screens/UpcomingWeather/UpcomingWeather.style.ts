19e15c822222ce1962763d1e898d64eae505da4b
Erdem Alptuğ Öksüz <81254185+erdemalptugoksuz@users.noreply.github.com>
2023-07-12 18:15:03 +0300

    Update Upcoming Weather screen

--- src/screens/UpcomingWeather/UpcomingWeather.style.ts
+++ src/screens/UpcomingWeather/UpcomingWeather.style.ts
@@ -1,3 +1,14 @@
 import {StyleSheet} from 'react-native';
 
-export default StyleSheet.create({});
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    marginHorizontal: 30,
+    marginTop: 30,
+    marginBottom: 10,
+  },
+});
--- src/screens/UpcomingWeather/UpcomingWeather.tsx
+++ src/screens/UpcomingWeather/UpcomingWeather.tsx
@@ -1,13 +1,26 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {SafeAreaView, FlatList, Text} from 'react-native';
 
 import styles from './UpcomingWeather.style';
+import {UpcomingItem} from '../../components';
 
-const UpcomingWeather = () => {
+const UpcomingWeather = ({weatherData}: any) => {
   return (
-    <View>
-      <Text>UpcomingWeather</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.title}>Upcoming Weather</Text>
+      <FlatList
+        data={weatherData}
+        renderItem={({item}) => (
+          <UpcomingItem
+            condition={item.weather[0].main}
+            dt_txt={item.dt_txt}
+            min={item.main.temp_min}
+            max={item.main.temp_max}
+          />
+        )}
+        keyExtractor={item => item.dt_txt}
+      />
+    </SafeAreaView>
   );
 };
 
