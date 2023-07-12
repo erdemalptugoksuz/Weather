ad75e47c1fa88cd94517bb4560a46975b5362a1a
Erdem Alptuğ Öksüz <81254185+erdemalptugoksuz@users.noreply.github.com>
2023-07-12 17:36:10 +0300

    Update Router.tsx

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,12 +1,38 @@
 import React from 'react';
-import {SafeAreaView, Text} from 'react-native';
+import {ActivityIndicator, View, StyleSheet, Text} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {useFetch} from './hooks/useFetch';
+
+import {Tabs} from './components';
 
 const Router = () => {
+  const [loading, error, weather] = useFetch();
+
+  console.log(error);
+
+  if (weather && weather.list && !loading) {
+    return (
+      <NavigationContainer>
+        <Tabs weather={weather} />
+      </NavigationContainer>
+    );
+  }
+
   return (
-    <SafeAreaView>
-      <Text>Router</Text>
-    </SafeAreaView>
+    <View style={styles.container}>
+      {error ? (
+        <Text>{error}</Text>
+      ) : (
+        <ActivityIndicator size={'large'} color={'blue'} />
+      )}
+    </View>
   );
 };
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    flex: 1,
+  },
+});
 
 export default Router;
